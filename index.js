37f2f8896b91e788eaaf94c1f6985bc3d236b3a5
hugosmoreira <hugo.n4@gmail.com>
2021-09-09 22:35:34 -0700

    update dialogdlow with amigodbot

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
+require('dotenv').config()
+
 const Discord = require('discord.js')
-//const fetch = require('node-fetch')
-//import fetch from 'node-fetch';
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
+const token = process.env.DISCORD_TOKEN;
 
-client.login('')
+const dialogflow = require('@google-cloud/dialogflow');
+const uuid = require('uuid');
 
-
-
-function getQuote() {
-    return fetch('https://zenquotes.io/api/quotes')
-    .then(res => {
-        return res.json
-    })
-    .then(data => {
-        return data[0]["q"] + " -" + data[0]["a"]
-    })
-}
 
 client.on('ready', () => {
-    console.log(`Logged in as $client.user.tag`)
+    console.log(`Logged in as ${client.user.tag}`)
 })
+
+async function replyMsg(textQuery) {
+    // A unique identifier for the given session
+    const projectId = process.env.PROJECT_ID;
+    const sessionId = uuid.v4();
+  
+    // Create a new session
+    const sessionClient = new dialogflow.SessionsClient();
+    const sessionPath = await sessionClient.projectAgentSessionPath(
+      projectId,
+      sessionId
+    );
+  
+    // The text query request.
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          // The query to send to the dialogflow agent
+          text: textQuery,
+          // The language used by the client (en-US)
+          languageCode: 'en-US',
+        },
+      },
+    };
+  
+     // Send request and log result
+     const responses = await sessionClient.detectIntent(request);
+     //   console.log("Detected intent");
+     const result = responses[0].queryResult;
+     console.log(`Query: ${result.queryText}`);
+     //   console.log(`  Response: ${result.fulfillmentText}`);
+     //   if (result.intent) {
+     //     console.log(`  Intent: ${result.intent.displayName}`);
+     //   } else {
+     //     console.log(`  No intent matched.`);
+     //   }
+ 
+     return await result.fulfillmentText;
+ }
 
 const replies = [
     'Hello World',
@@ -31,15 +62,22 @@ const replies = [
 ]
 
 client.on('message', msg => {
-    if (msg.author.bot) return
+    console.log(msg.author.bot)
+    if (!msg.author.bot) {
 
-    if( msg.content === '$inspire') {
-        getQuote().then(quote => msg.channel.send(quote))
+        replyMsg(msg.content).then((res) => console.log(res))
+
+        msg.reply(msg.content)
+
     }
+    
 })
 
+
+
+
 client.on('message', msg => {
-    if( msg.content === 'Hello' || msg.content === 'hello') {
+    if( msg.content === 'Oi' || msg.content === 'Oi') {
         const index = Math.floor(Math.random() * replies.length)
         msg.reply(replies[index])
     }
@@ -52,8 +90,6 @@ client.on('message', msg => {
     }
 })
 
-// Error handling
-client
-  .on("error", console.error)
-  .on("warn", console.warn)
-  .on("debug", console.log);
+
+
+  client.login(token)
